3f2a34f385fb6037e7bd4d9d9d15ad2700d4af09
manasses.silva <manasses3mm@gmail.com>
2021-06-07 11:17:49 -0300

    change main text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          GitHub Actions
         </p>
         <a
           className="App-link"
