cd3da31efb59d153b9a8680579d35bf1ccd53a81
manasses.silva <manasses3mm@gmail.com>
2021-06-07 11:39:48 -0300

    implement task #1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          GitHub Actions
+          GitHub Actions - New Workflow
         </p>
         <a
           className="App-link"
